7108ebb242fa32cc6eb8a808735c603ed42267de
SwiftCoderJoe <coderjoe05@gmail.com>
2020-05-08 21:10:11 -0400

    Fix GMobileAds

--- Podfile
+++ Podfile
@@ -3,12 +3,15 @@
 
 target 'memeinator3000' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # use_modular_headers! 
+  # CHANGE THIS TO use_frameworks! WHEN USING REAL iOS DEV ACCOUNT
   use_frameworks!
 
   # Pods for memeinator3000
   pod 'IQKeyboardManagerSwift'
   pod 'Firebase/Core'
-  pod 'Firebase/AdMob'
+  pod 'Firebase/Analytics'
+  pod 'Google-Mobile-Ads-SDK'
   pod 'Fabric'
   pod 'Crashlytics'
   
--- memeinator3000/AppDelegate.swift
+++ memeinator3000/AppDelegate.swift
@@ -16,13 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        IQKeyboardManager.shared.enable = true
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-3940256099942544/2934735716")
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
